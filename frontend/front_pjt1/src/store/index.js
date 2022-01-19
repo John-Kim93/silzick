04af1a18bf82af3dcf181f85d8448b236e357a9b
YJM <yoonjm7063@gmail.com>
2022-01-19 13:31:25 +0900

    FEAT skeleton_code

--- frontend/front_pjt1/src/store/index.js
+++ frontend/front_pjt1/src/store/index.js
@@ -5,6 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    admin_post : {
+      id : 1,
+      title : 'test1_admin_title',
+      content : 'test1_admin_content',
+      created_at : '22.01.19',
+    },
+    user_post : {
+      id : 1,
+      title : 'test1_user',
+      created_at : '22.01.19',
+    }
   },
   mutations: {
   },
