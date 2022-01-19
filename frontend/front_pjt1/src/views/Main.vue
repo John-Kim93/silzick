36adc2a490e3c36ce656c31ca548db823912adfa
John Kim <dhkdhd123@naver.com>
2022-01-19 13:33:57 +0900

    FIX combine vue skeleton codes

--- frontend/front_pjt1/src/views/Main.vue
+++ frontend/front_pjt1/src/views/Main.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div>
+    <img src="https://ifh.cc/g/g1KuX0.jpg" id="bg" alt="bgImg">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
-  name: 'Home',
+  name: 'Main',
   components: {
     
   }
 }
 </script>
+
+<style scoped>
+  #bg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
