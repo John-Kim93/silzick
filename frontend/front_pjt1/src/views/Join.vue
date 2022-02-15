5c6576d5f67b4ee08d1def3b819071af1cab1a2a
YJM <yoonjm7063@gmail.com>
2022-02-15 11:54:25 +0900

    FEAT invite&join room

--- frontend/front_pjt1/src/views/Join.vue
+++ frontend/front_pjt1/src/views/Join.vue
@@ -3,14 +3,14 @@
     <div id="base-border" class="container d-flex flex-row justify-content-center align-content-center row col-7" style="height:40vh">
       <div class="col-11 row d-flex align-content-center" style="height:13vh">
         <h3 id="base-font" class="m-2">Nickname</h3>
-          <input
-          id="base-border"
-          type="text"
-          v-model="nickname"
-          class="form-control m-2"
-          placeholder="사용하실 닉네임을 입력해주세요."
-          required
-          >
+        <input
+        id="base-border"
+        type="text"
+        v-model="nickname"
+        class="form-control m-2"
+        placeholder="사용하실 닉네임을 입력해주세요."
+        required
+        >
       </div>
       <div class="col-8 row d-flex align-content-center" style="height:13vh">
         <button id="btn-color" class="btn mt-4 px-5" @click="nicknameUpdate(nickname)">참가</button>
